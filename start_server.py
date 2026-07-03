5aab722f2e51dc802be0bfe0fef1cf2919f65ff8
Reza Zusman <121447484+rezazusman@users.noreply.github.com>
2026-07-04 03:00:41 +0800

    start_server.py

--- start_server.py
+++ start_server.py
@@ -3,17 +3,20 @@ import sys
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PYTHON = sys.executable
 
-python = sys.executable
+print("="*50)
+print("PLTMG AI SERVER")
+print("="*50)
 
 subprocess.Popen(
-    [python, os.path.join(BASE_DIR, "main.py")],
+    [PYTHON, os.path.join(BASE_DIR, "main.py")],
     creationflags=subprocess.CREATE_NEW_CONSOLE
 )
 
 subprocess.Popen(
-    [python, os.path.join(BASE_DIR, "history_writer.py")],
+    [PYTHON, os.path.join(BASE_DIR, "history_writer.py")],
     creationflags=subprocess.CREATE_NEW_CONSOLE
 )
 
-print("PLTMG AI Server Started")
+print("Server berhasil dijalankan.")
